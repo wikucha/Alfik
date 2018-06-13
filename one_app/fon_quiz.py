a637184cb98b658046d5b28d79b7c4e99c33691c
wikucha <wikucha77@wp.pl>
2018-06-13 15:03:16 +0200

    poprawa
    poprawalayout

--- one_app/fon_quiz.py
+++ one_app/fon_quiz.py
@@ -13,7 +13,7 @@ def action(app, button, color, buttons, wynik, czy_prawda):
         button.background_color=color
 
         if czy_prawda:
-            #wynik.score += 1
+            wynik.score = 1
             pass
 
         for B in buttons:
@@ -52,14 +52,14 @@ class FonquizScreen(BaseScreen):
             wybrane_litery.append(litera)
 
             random.shuffle(wybrane_litery)
-            layout.score = NumericProperty(0)
-
+            layout.score = str(NumericProperty(0))
+            layout.ids.wynik=layout.score
             for Przycisk, wybrana_litera in zip(buttons, wybrane_litery):
                 Przycisk.text = wybrana_litera
                 if litera == wybrana_litera:
-                    Przycisk.bind(on_release=action(self, Przycisk, odp_true, buttons, layout.ids.wynik, True))
+                    Przycisk.bind(on_release=action(self, Przycisk, odp_true, buttons, layout, True))
                 else:
-                    Przycisk.bind(on_release=action(self, Przycisk, odp_false, buttons, layout.ids.wynik, False))
+                    Przycisk.bind(on_release=action(self, Przycisk, odp_false, buttons, layout, False))
 
             layout.ids.play_sound.bind(on_release= play_sound(litera_sound))
 
