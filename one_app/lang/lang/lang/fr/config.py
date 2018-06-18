57cbb629a99fc64d85ed3f331963ed6f2ad1cd45
Shinushining <34514875+Shinushining@users.noreply.github.com>
2018-06-18 14:01:59 +0200

    drobne zmiany

---
+++ one_app/lang/lang/lang/fr/config.py
@@ -0,0 +1,19 @@
+
+translate = {
+ 'ai': {'translation': 'e', 'word': 'éclair', 'sound':'sound/franc/ai.mp3'},
+ 'au': {'translation': 'o', 'word': 'auteur', 'sound':''},
+ 'ou': {'translation': 'u', 'word': 'boutique', 'sound':''},
+ 'oi': {'translation': 'ła', 'word': 'anchois', 'sound':''},
+ 'gn': {'translation': 'ń', 'word': 'montagne', 'sound':''},
+ 'ch': {'translation': 'sz', 'word': 'chambre', 'sound':''},
+ 'ç': {'translation': 's', 'word': 'garçon', 'sound':''},
+ 'j': {'translation': 'ż', 'word': 'jeune', 'sound':''},
+ 'œ ': {'translation': 'e/y', 'word': 'sœur', 'sound':''},
+ 'ph': {'translation': 'f', 'word': 'phase', 'sound':''},
+ 'on': {'translation': 'ą', 'word': 'déclinaison', 'sound':''},
+ 'om': {'translation': 'ą', 'word': 'ombre', 'sound':''},
+ 'an': {'translation': 'ą', 'word': 'ange', 'sound':''},
+ 'am': {'translation': 'ą', 'word': 'ambiance', 'sound':''},
+ 'in': {'translation': 'ę', 'word': 'merlin', 'sound':''},
+ 'im': {'translation': 'ę', 'word': 'limbe', 'sound':''},
+ }
---
+++ one_app/ulubione/learn.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from kivy.lang import builder
+from kivy.uix.carousel import Carousel
+from kivy.uix.screenmanager import Screen
+
+from base_screen import BaseScreen
+from tools import przycisk_ulubione, play_sound
+
+# zmiana litery
+def action(label, org, translate):
+    def change(obj):
+        if label.text == org:
+            label.text = translate
+            label.font_name='fonts/roboto.ttf'
+        else:
+            label.text = org
+            label.font_name='fonts/DroidSansJapanese.ttf'
+    return change
+
+# karuzela
+class LearnScreen(BaseScreen):
+    def __init__(self, name):
+        super(Screen, self).__init__(name=name)
+
+    def on_enter(self):
+
+        carousel = Carousel(direction='right')
+
+        # tłumaczenie słowa-połaczenie z layoutem
+        for litera in self.lang["translate"]:
+            litera_tlumaczenie = self.lang["translate"][litera]["translation"]
+            layout = builder.Builder.load_file("learn_layout.kv")
+            carousel.add_widget(layout)
+
+            slowo = layout.ids.slowo
+            slowo.text = self.lang["translate"][litera]["word"]
+
+            duza_litera = layout.ids.duza_litera
+            duza_litera.text = litera
+
+            # Podpięte działania
+            layout.ids.zamien_litere.bind(
+                on_release=action(duza_litera, litera, litera_tlumaczenie)
+            )
+
+            # utrzymanie przycisku
+            polub = layout.ids.fav
+            przycisk_ulubione(polub, litera, self.ulubione)
+
+            # Dźwięk
+            litera_sound = self.lang["translate"][litera]["sound"]
+            layout.ids.play_sound.bind(on_release=play_sound(litera_sound))
+
+        self.add_widget(carousel)
